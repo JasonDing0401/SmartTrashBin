3778879e92de90061a4a4dc98aa5657ca92f16e5
JasonDing0401 <53387039+JasonDing0401@users.noreply.github.com>
2019-10-01 22:11:09 -0700

    Add files via upload

---
+++ TrashBin/gvision_test/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+GOOGLE_APPLICATION_CREDENTIALS=credential.json proxychains4 python3 main.py
+
---
+++ TrashBin/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+GOOGLE_APPLICATION_CREDENTIALS=credential.json proxychains4 python3 main.py
